0389895d6f98b28ccd2b4815513439c4ef810403
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-01-29 12:30:08 +0400

    signal, clearinterval

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
 import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import FormikSample from './sample/FormikSample'
 import { ToDoProvider } from './store/todoContext'
 import TodoList from './screens/TodoList'
 import AddTodo from './screens/AddTodo'
+import axios from 'axios'
 
 const App = () => {
+
+
+  useEffect(() => {
+
+    const controller = new AbortController();
+
+    axios.get('https://northwind.vercel.app/api/products', {
+      signal: controller.signal
+    }).then(function (response) {
+      //...
+    });
+
+    let interval = setInterval(() => {
+      //get data
+    }, 1000)
+
+    
+
+
+    return () => {
+      controller.abort();
+      clearInterval(interval);
+    }
+  }, [])
+
+
+
   return (
     <SafeAreaView>
-        <ToDoProvider>
-          <AddTodo/>
-          <TodoList/>
-        </ToDoProvider>
+      <ToDoProvider>
+        <AddTodo />
+        <TodoList />
+      </ToDoProvider>
     </SafeAreaView>
   )
 }
