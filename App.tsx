f50b7129b1048e373a4be9cdfaf5d7f9ade6626d
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-01-29 11:32:43 +0400

    project init, storage, formik sample

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import FormikSample from './sample/FormikSample'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+        <FormikSample/>
+    </SafeAreaView>
+  )
+}
+
+export default App
